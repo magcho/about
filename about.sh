17477401222391376dfb0a50b8f59e05ae67e7ac
magcho <mail@magcho.com>
2021-07-24 11:51:58 +0900

    Update github open

--- about.sh
+++ about.sh
@@ -13,37 +13,37 @@
 
 
 Usage(){
-echo  "usage: curl -Ls about.magcho.com | sh
+    echo  "usage: curl -Ls about.magcho.com | sh
    magcho's self-introduction
 
    How to execute subcommands
-      Ex.) curl -Ls about.magcho.com | sh -s help
+      e.g.) curl -Ls about.magcho.com | sh -s help
    "
 }
 Help(){
-echo "subcommands:
+    echo "subcommands:
    help     Display the help message
    more     Display full the magcho's introduction
    tw       Visit to magcho's twitter
    hp       Visit to magcho.com"
 }
 Intro(){
-echo  " |        \033[32mName:\033[m magcho                         |
+    echo  " |        \033[32mName:\033[m magcho                         |
  |     \033[034mTwitter:\033[039m https://twitter.com/magcho0527 |
  |        \033[36mMail:\033[m mail@magcho.com                |
  |          \033[31mHP:\033[m https://magcho.com             |"
 }
 IntroMore(){
 
-if echo "$EDITOR" | grep 'emacs' > /dev/null; then
-   EDITOR_MESS="     (You'er emacser!! Let's be friends!)    |"
-elif echo "$EDITOR" | grep 'vim' > /dev/null; then
-   EDITOR_MESS="   (YOU ARE VIMMER??? SHALL WE USE EMACS?)   |"
-else
-   EDITOR_MESS="            (Shall we use Emacs?)            |"
-fi
+    if echo "$EDITOR" | grep 'emacs' > /dev/null; then
+        EDITOR_MESS="     (You'er emacser!! Let's be friends!)    |"
+    elif echo "$EDITOR" | grep 'vim' > /dev/null; then
+        EDITOR_MESS="   (YOU ARE VIMMER??? SHALL WE USE EMACS?)   |"
+    else
+        EDITOR_MESS="            (Shall we use Emacs?)            |"
+    fi
 
-echo  " |        Blog: https://blog.magcho.com        |
+    echo  " |        Blog: https://blog.magcho.com        |
  |      Github: https://github.com/magcho      |
  |        Name: Suetake Dai                    |
  |         Age: 22                             |
@@ -53,40 +53,48 @@ echo  " |        Blog: https://blog.magcho.com        |
  |$EDITOR_MESS"
 }
 Open(){
-   if [ $1 = tw ]; then
-      open https://twitter.com/magcho0527
-   elif [ $1 = hp ]; then
-      open https://magcho.com
-   fi
+    if [ $1 = tw ]; then
+        echo 'open twitter: https://twitter.com/magcho0527'
+        open https://twitter.com/magcho0527
+    elif [ $1 = hp ]; then
+        echo 'open HP: https://magcho.com'
+        open https://magcho.com
+    elif [ $1 = gh ]; then
+        echo 'open GitHub: https://github.com/magcho/'
+        open https://github.com/magcho/
+    fi
 }
 PrintLine(){
-   echo " ==============================================="
+    echo " ==============================================="
 }
 
 
 
 case $1 in
-   help)
-      Usage
-      Help
-      ;;
-   more)
-      Usage
-      PrintLine
-      Intro
-      IntroMore
-      PrintLine
-      ;;
-   tw)
-      Open tw
-      ;;
-   hp)
-      Open hp
-      ;;
-   *)
-      Usage
-      PrintLine
-      Intro
-      PrintLine
-      ;;
+    help)
+        Usage
+        Help
+        ;;
+    more)
+        Usage
+        PrintLine
+        Intro
+        IntroMore
+        PrintLine
+        ;;
+    tw)
+        Open tw
+        ;;
+    hp)
+        Open hp
+        ;;
+    gh)
+        Open gh
+        ;;
+    *)
+        Usage
+        PrintLine
+        Intro
+        PrintLine
+        ;;
 esac
